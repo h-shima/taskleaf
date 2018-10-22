926666c143ca0ba76150d9b439790e4762bbfc50
hirokioshima <geet1876@gmail.com>
2018-10-22 22:39:58 +0900

    タスク作成時の確認ページを削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,18 +13,8 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def confirm_new
-    @task = current_user.tasks.new(task_params)
-    render :new unless @task.valid?
-  end
-
   def create
     @task = current_user.tasks.new(task_params)
-
-    if params[:back].present?
-      render :new
-      return
-    end
 
     if @task.save
       TaskMailer.creation_email(@task).deliver_now
